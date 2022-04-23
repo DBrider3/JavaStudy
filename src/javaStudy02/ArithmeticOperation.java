e9429ad262fd301fcbe5411d9335fe3cf35949cb
Soksurim <briochefavori@gmail.com>
2022-04-23 15:30:33 +0900

    javaStudy02 Complete

--- src/javaStudy02/ArithmeticOperation.java
+++ src/javaStudy02/ArithmeticOperation.java
@@ -1,4 +1,25 @@
-package javaStudy02;
+package com.javaStudy02;
 
 public class ArithmeticOperation {
+
+    private double num1;
+    private double num2;
+
+    public void setNums(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double plus(){
+        return num1 + num2;
+    }
+    public double minus(){
+        return num1 - num2;
+    }
+    public double mul(){
+        return num1 * num2;
+    }
+    public double div(){
+        return num1 / num2;
+    }
 }
--- src/javaStudy02/ArithmeticOperationTest.java
+++ src/javaStudy02/ArithmeticOperationTest.java
@@ -1,7 +1,12 @@
-package javaStudy02;
+package com.javaStudy02;
 
 public class ArithmeticOperationTest {
     public static void main(String[] args) {
-
+        ArithmeticOperation ao = new ArithmeticOperation();
+        ao.setNums(3, 5);
+        System.out.println(ao.plus());
+        System.out.println(ao.minus());
+        System.out.println(ao.mul());
+        System.out.println(ao.div());
     }
 }
